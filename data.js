77868d731ecb7480250e3337baeac941b3c5d1d1
AmelieLT <amelie_letron@yahoo.fr>
2022-12-05 12:27:07 +0100

    creacion data.js

---
+++ data.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id: 1,
+        nombre: "Zapacuario",
+        cantidad: 1,
+        desc: "zapato y acuario",
+        precio: 70,
+        img: "./images/zapacuario.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Arnes para gallo",
+        cantidad: 1,
+        desc: "Arnes para Gallo",
+        precio: 20,
+        img: "./images/arnesgallo.jpg" 
+    },
+    {
+        id: 3,
+        nombre: "Secador manos libres",
+        cantidad: 1,
+        desc: "Secador manos libres",
+        precio: 99,
+        img: "./images/zapacuario.jpg" 
+    },
+    {
+        id: 4,
+        nombre: "Pezchancla",
+        cantidad: 1,
+        desc: "Chanclas de pez",
+        precio: 70,
+        img: "./images/pezchancla.jpeg" 
+    },
+    {
+        id: 5,
+        nombre: "Tocador para puerta",
+        cantidad: 1,
+        desc: "zapato y acuario",
+        precio: 90,
+        img: "./images/zapacuario.jpg" 
+    },
+    {
+        id: 6,
+        nombre: "MiniGolf baño",
+        cantidad: 1,
+        desc: "zapato y acuario",
+        precio: 70,
+        img: "./images/minigolf.png" 
+    }
+    ];
