3e6e433cf081c637977696f20bf1bbbbbcbee320
juliana montano <zjulim89@gmail.com>
2022-12-05 15:00:13 +0100

    borrado alguns comentarios

--- app.js
+++ app.js
@@ -1,79 +1,3 @@
-/*const stockProductos = [
-    {
-        id: 1,
-        nombre: "Zapacuario",
-        cantidad: 1,
-        desc: "zapato y acuario",
-        precio: 70,
-        img: "./images/zapacuario.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Arnes para gallo",
-        cantidad: 1,
-        desc: "Arnes para Gallo",
-        precio: 20,
-        img: "./images/arnesgallo.jpg" 
-    },
-    {
-        id: 3,
-        nombre: "Secador manos libres",
-        cantidad: 1,
-        desc: "Secador manos libres",
-        precio: 99,
-        img: "./images/zapacuario.jpg" 
-    },
-    {
-        id: 4,
-        nombre: "Pezchancla",
-        cantidad: 1,
-        desc: "Chanclas de pez",
-        precio: 70,
-        img: "./images/pezchancla.jpeg" 
-    },
-    {
-        id: 5,
-        nombre: "Tocador para puerta",
-        cantidad: 1,
-        desc: "zapato y acuario",
-        precio: 90,
-        img: "./images/zapacuario.jpg" 
-    },
-    {
-        id: 6,
-        nombre: "MiniGolf baño",
-        cantidad: 1,
-        desc: "zapato y acuario",
-        precio: 70,
-        img: "./images/minigolf.png" 
-    }
-    ];
-
-let carrito = []
-
-
-const contenedor = document.querySelector('#container')
-
-stockProductos.forEach((prod) => {
-    const {id, nombre, precio, desc, img, cantidad} = prod
-    contenedor.innerHTML += `
-    <div class="card">
-    <img src="${img}" class="card-img-top" alt="Zapato y Acuario">
-    <div class="card-body">
-      <h5 class="card-title">${nombre}</h5>
-      <p class="card-text">${precio}</p>
-      <p class="card-text">Cantidad:${cantidad}</p>
-      <button onclick="agregarProducto(${id})" type="button" class="btn btn-warning">¡Lo Quiero!</button>
-    </div>
-  </div>
-   
-    `
-})
-
-function agregarProducto(id){
-    const item = stockProductos.find((prod) => prod.id ===id)
-    console.log(carrito)
-}*/
 
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
@@ -95,19 +19,6 @@ let countProducts = document.querySelector('#product-counter');
 
 
 let minusPlus = document.querySelector('.glyphicon')
-
-/*const processOrder = document.getElementById('.btn-go-to-cart');
-
-
-
-processOrder.addEventListener('click', e => {
-    product.btnGotoCart(e)
-        e.prevenDefault();
-        location.href = 'basket.html';
-
-});*/
-
-
 
 
 productsList.addEventListener('click', e => {
